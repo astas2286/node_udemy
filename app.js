857b4e1892f5e4d50d52ffda42e288e1d95f064c
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-11-10 23:55:27 +0200

    lecture 68 eslint + prettier

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-const express = require('express')
-const morgan = require('morgan')
+const express = require('express');
+const morgan = require('morgan');
 
-const tourRouter = require('./routes/tourRoutes')
-const userRouter = require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-const app = express()
+const app = express();
 
 // 1 - MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'))
+  app.use(morgan('dev'));
 }
 
-app.use(express.json())
-app.use(express.static(`${__dirname}/public`))
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log('Hello from the middleware!')
-    next()
-})
+  console.log('Hello from the middleware!');
+  next();
+});
 
 app.use((req, res, next) => {
-    req.requestTime = new Date().toISOString()
-    next()
-})
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
-app.use('/api/v1/tours', tourRouter)
-app.use('/api/v1/users', userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+const dotenv = require('dotenv');
 
-const app = require('./app')
+dotenv.config({
+  path: './config.env',
+});
 
-const port = process.env.PORT || 3007
+const app = require('./app');
+
+const port = process.env.PORT || 3007;
 app.listen(port, () => {
-    console.log(`App is running on port ${port}`);
-})
-
+  console.log(`App is running on port ${port}`);
+});
