579bc1cd45e0460299461174ad84b13fa0d72ff8
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-12-03 21:46:55 +0200

    mongoDB setup

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
+const mongoose = require('mongoose')
 const dotenv = require('dotenv');
+const app = require('./app')
 
 dotenv.config({
   path: './config.env',
 });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con => {
+  console.log(con.connection)
+  console.log('CONNECTED TO DB');
+})
 
 const port = process.env.PORT || 3007;
 app.listen(port, () => {
