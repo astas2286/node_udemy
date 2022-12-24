9645d2ce78dd6a5c062023e25247e74e78b82603
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2022-12-24 18:14:21 +0200

    93

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -2,15 +2,17 @@ const Tour = require('./../models/tourModel')
 
 exports.getAllTours = async (req, res) => {
 
-    try{const tours = await Tour.find()
+    try {
+        const tours = await Tour.find()
 
-    res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
-    })} catch(err){
+        res.status(200).json({
+            status: 'success',
+            results: tours.length,
+            data: {
+                tours
+            }
+        })
+    } catch (err) {
         res.status(404).json({
             status: 'fail',
             message: err
@@ -19,22 +21,22 @@ exports.getAllTours = async (req, res) => {
 }
 
 exports.getTour = async (req, res) => {
-try {
-    const tour = await Tour.findById(req.params.id)
+    try {
+        const tour = await Tour.findById(req.params.id)
 
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tours: tour
-        }
-    })
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tours: tour
+            }
+        })
 
-} catch (err) {
-    res.status(404).json({
-        status: 'fail',
-        message: err
-    })
-}
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+    }
 }
 
 exports.createTour = async (req, res) => {
@@ -51,25 +53,49 @@ exports.createTour = async (req, res) => {
             }
         })
     } catch (err) {
-res.status(400).json({
-    status: 'fail',
-    message: 'Invalid data sent!'
-})
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        })
     }
 }
 
-exports.updateTour = (req, res) => {
-    res.status(200).json({
-        status: 'success',
-        data: {
-            tour: '<Updated tour here...>'
-        }
-    })
+exports.updateTour = async (req, res) => {
+    try {
+        const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                tour
+            }
+        })
+
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+
+    }
 }
 
-exports.deleteTour = (req, res) => {
-    res.status(204).json({
-        status: 'success',
-        data: null
-    })
+exports.deleteTour = async (req, res) => {
+    try {
+        await Tour.findByIdAndDelete(req.params.id)
+        // don`t need to save in a const, we don`t send anything to the client
+        res.status(204).json({
+            status: 'success',
+            data: null
+        })
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+
+    }
 }
