ca204c676146ea9242138d4aa93afe5604375371
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-03 09:48:25 +0200

    96 filtering

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -6,16 +6,20 @@ const Tour = require('./../models/tourModel')
 exports.getAllTours = async (req, res) => {
 
     try {
-// HERE WE BUILD QUERY
+        // HERE WE BUILD QUERY
+        // 1) Filtering
         const queryObj = {
             ...req.query
         }
         const excludedFields = ['page', 'sort', 'limit', 'fields']
         excludedFields.forEach(el => delete queryObj[el])
 
-        console.log(req.query, queryObj)
+        // 2) Advanced Filtering
+        let queryStr = JSON.stringify(queryObj)
+        queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`)
+        console.log(JSON.parse(queryStr))
 
-        const query = Tour.find(queryObj)
+        const query = Tour.find(JSON.parse(queryStr))
 
         // const tours = await Tour.find({
         //     duration: 5,
