8820e0bcb386c983cc2665f605a177462ee2b26e
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-05 15:38:28 +0200

    102 agrigation

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -119,3 +119,67 @@ exports.deleteTour = async (req, res) => {
 
     }
 }
+
+exports.getTourStats = async (req, res) => {
+    try {
+        const stats = await Tour.aggregate([{
+                $match: {
+                    ratingsAverage: {
+                        $gte: 4.5
+                    }
+                }
+            },
+            {
+                $group: {
+                    // _id: '$ratingsAverage', you can use any parameter
+                    _id: {
+                        $toUpper: '$difficulty'
+                    },
+                    numTours: {
+                        $sum: 1
+                    },
+                    numRatings: {
+                        $sum: '$ratingsQuantity'
+                    },
+                    avgRating: {
+                        $avg: '$ratingsAverage'
+                    },
+                    avgPrice: {
+                        $avg: '$price'
+                    },
+                    minPrice: {
+                        $min: '$price'
+                    },
+                    maxPrice: {
+                        $max: '$price'
+                    }
+                }
+            },
+            {
+                $sort: {
+                    avgPrice: 1
+                }
+            }
+            // , We can exclude results with "EASY" id with $match
+            // {
+            //     $match: {
+            //         _id: {
+            //             $ne: 'EASY'
+            //         }
+            //     }
+            // }
+        ])
+        res.status(200).json({
+            status: 'success',
+            data: {
+                stats
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        })
+
+    }
+}
