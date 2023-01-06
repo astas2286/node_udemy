3c6fb48b431b0c385c65b01bda4fd36cfcbcdfbc
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-06 11:38:08 +0200

    112 handling incorrect URLs

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// code below must be in the end of the code to correctly catch all URLs
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Cant find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
--- models/tourModel.js
+++ models/tourModel.js
@@ -123,8 +123,6 @@ tourSchema.pre(/^find/, function (next) {
 })
 
 tourSchema.post(/^find/, function (docs, next) {
-  console.log(`Query took ${Date.now() - this.start} ms!`);
-  console.log('docs', docs)
   next()
 })
 
