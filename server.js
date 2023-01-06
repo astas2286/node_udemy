c1c4baccb97835cd7b1c20982e53c1a3c8167c74
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-06 17:15:03 +0200

    123 catching uncaught and unhandled errors

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv');
-const app = require('./app')
+
+process.on('uncaughtException', err => {
+  console.log('uncaughtException!!! Shutting down...')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
 
 dotenv.config({
   path: './config.env',
 });
+const app = require('./app')
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD)
 
@@ -15,6 +21,14 @@ mongoose.connect(DB, {
 }).then(() => console.log('CONNECTED TO DB SUCCESSFUL!'))
 
 const port = process.env.PORT || 3007;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
-});
+const server = app.listen(port, () => {
+  console.log(`App is running on port ${port}`)
+})
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION!!! Shutting down...')
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1)
+  })
+})
