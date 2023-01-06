359d56f07658ecda775c3cefbda9c1dc08339327
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-06 16:29:55 +0200

    121 handling validation errors

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require("../utils/appError")
+
+const handleCastErrorDB = err => {
+    const message = `Invalid ${err.path}: ${err.value}`
+    return new AppError(message, 400)
+}
+
+const handleDuplicateFieldsDB = err => {
+    const value = err.errmsg.match(/(["'])(?:(?=(\\?))\2.)*?\1/)[0]
+    const message = `Duplicate field value ${value}. Please use another name`
+    return new AppError(message, 400)
+}
+
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message)
+
+    const message = `Invalid input data. ${errors.join('. ')}`
+    return new AppError(message, 400)
+}
+
 const sendErrorDev = (err, res) => {
     res.status(err.statusCode).json({
         status: err.status,
@@ -31,9 +51,13 @@ module.exports = (err, req, res, next) => {
     err.statusCode = err.statusCode || 500
     err.status = err.status || 'error'
 
-    if (process.env === 'development') {
+    if (process.env.NODE_ENV === 'development') {
         sendErrorDev(err, res)
-    } else if (process.env === 'production') {
-        sendErrorProd(err, res)
+    } else if (process.env.NODE_ENV === 'production') {
+        let error = Object.assign(err)
+        if (err.name === 'CastError') error = handleCastErrorDB(error)
+        if (err.code === 11000) error = handleDuplicateFieldsDB(error)
+        if (err._message === 'Validation failed') error = handleValidationErrorDB(error)
+        sendErrorProd(error, res)
     }
 }
