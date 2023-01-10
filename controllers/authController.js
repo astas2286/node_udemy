dc6a61a4b99e747e14f395c36fb75a1622c2c579
ASTOUND\s.afanasiev <s.afanasiev@astoundcommerce.com>
2023-01-10 17:43:24 +0200

    134. Authorization: User Roles and Permissions

--- controllers/authController.js
+++ controllers/authController.js
@@ -92,3 +92,13 @@ exports.protect = catchAsync(async (req, res, next) => {
     req.user = currentUser
     next()
 })
+
+exports.restrictTo = (...roles) => {
+    return (req,res,next) =>{
+        //roles is an array ['admin', 'lead-guide']
+        if(!roles.includes(req.user.role)){
+            return next(new AppError('You have no permission for this action!', 403))
+        }
+        next()
+    }
+}
