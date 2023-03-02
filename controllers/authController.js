29f98e858dedce645b77ebe800c5a014287be37c
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-02 13:31:21 +0200

    142 sending JWT via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,17 @@ const signToken = id => {
 
 const createSendToken = (user, statusCode, res) => {
     const token = signToken(user._id)
+    const cookieOptions = {
+        expires: new Date(
+            Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+        ),
+        httpOnly: true
+    }
+    if (process.env.NODE_ENV === 'production') cookieOptions.secure = true // to send cookie via encrypted connection using https
+
+    res.cookie('jwt', token, cookieOptions)
+
+    user.password = undefined // to remove the password from output
 
     res.status(statusCode).json({
         status: 'success',
