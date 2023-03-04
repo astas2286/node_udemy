e3076423865999b3a1fca7a9a8d27aceb9e0d50c
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-04 17:13:22 +0200

    150 modeling tour guides: child referencing

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-const User = require('./userModel')
-
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -103,9 +101,14 @@ const tourSchema = new mongoose.Schema({
       day: Number
     }
   ],
-  guides: Array
-}
-  , {
+  guides: [
+    {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User'
+    }
+  ]
+},
+  {
     toJSON: { virtuals: true },
     toObject: { virtuals: true },
   })
@@ -122,11 +125,12 @@ tourSchema.pre('save', function (next) {
   next()
 })
 
-tourSchema.pre('save', async function (next) {
+/*THIS CODE IS USEFUL FOR EMBEDDING 
+tourSchema.pre('save', async function(next) {
   const guidesPromises = this.guides.map(async id => await User.findById(id))
   this.guides = await Promise.all(guidesPromises)
   next()
-})
+})*/
 
 // tourSchema.pre('save', function(next){
 //   console.log('will save doc...')
