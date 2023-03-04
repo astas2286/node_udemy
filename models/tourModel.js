35e78d05ace9f93084bafeb52d9daa0d96919bd9
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-04 16:58:53 +0200

    149 modeling tour guides: embedding

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
-const validator = require('validator')
+const User = require('./userModel')
+
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -101,12 +102,13 @@ const tourSchema = new mongoose.Schema({
       description: String,
       day: Number
     }
-  ]
+  ],
+  guides: Array
 }
-, {
-  toJSON: { virtuals: true },
-  toObject: { virtuals: true },
-})
+  , {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  })
 
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7
@@ -117,6 +119,12 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {
     lower: true
   })
+  next()
+})
+
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id))
+  this.guides = await Promise.all(guidesPromises)
   next()
 })
 
