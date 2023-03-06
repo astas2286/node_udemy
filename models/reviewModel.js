728768f34e9a2f001d9cf507eabc753087ad096f
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-06 16:36:29 +0200

    166 calculating avarage rating on tours - part1

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const Tour = require('./tourModel')
 
 const reviewSchema = new mongoose.Schema({
     review: {
@@ -41,10 +42,39 @@ reviewSchema.pre(/^find/, function (next) {
     // })
 
     this.populate({
-            path: 'user',
-            select: 'name photo' //to include only name & photo
-        })
+        path: 'user',
+        select: 'name photo' //to include only name & photo
+    })
     next()
+})
+
+// aggregator to calculate average rating and number of ratings
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+    const stats = await this.aggregate([
+        {
+            $match: { tour: tourId }
+        },
+        {
+            $group: {
+                _id: '$tour',
+                nRating: { $sum: 1 },
+                avgRating: { $avg: '$rating' }
+            }
+        }
+    ])
+    console.log(stats)
+
+    await Tour.findByIdAndUpdate(tourId, {
+        ratingsQuantity: stats[0].nRating,
+        ratingsAverage: stats[0].avgRating
+    })
+}
+
+reviewSchema.post('save', function () {
+    //this points to current review
+    // we use this.constructor because Rewiev variable is not yet defined 
+    //we use .post instead of pre to count this last review too
+    this.constructor.calcAverageRatings(this.tour) 
 })
 
 const Review = mongoose.model('Review', reviewSchema)
