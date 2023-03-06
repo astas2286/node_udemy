0c7932b8a308ed941c6a977926be324a236039c3
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-06 12:30:16 +0200

    162 adding a /me endpoint

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,6 +13,11 @@ const filterObj = (obj, ...allowedFields) => {
     return newObj
 }
 
+exports.getMe = (req, res, next) => {
+    req.params.id = req.user.id  // to create endpoint where user can retive his own data
+    next()
+}
+
 exports.updateMe = catchAsync(async (req, res, next) => {
     //1) Create error if user POSTs password data
     if (req.body.password || req.body.passwordConfirm) {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -11,6 +11,7 @@ router.patch('/resetPassword/:token', authController.resetPassword)
 
 router.patch('/updateMyPassword/:token', authController.protect, authController.updatePassword)
 
+router.get('/me', authController.protect, userController.getMe, userController.getUser)
 router.patch('/updateMe', authController.protect, userController.updateMe)
 router.delete('/deleteMe', authController.protect, userController.deleteMe) //user is actually not deleted, but it`s ok to use DELETE http method here
 
