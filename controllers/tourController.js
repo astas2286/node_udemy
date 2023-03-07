da4575300cc7f477b76023aad7e61ebba4764bb9
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-07 15:07:23 +0200

    170 geospatial aggregation: calculating distances

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -128,5 +128,44 @@ exports.getToursWithin = catchAsync(async (req, res, next) => {
         data: {
             data: tours
         }
-    });
+    })
+})
+
+exports.getDistances = catchAsync(async (req, res, next) => {
+    const { latlng, unit } = req.params
+    const [lat, lng] = latlng.split(',')
+
+    const multiplier = unit === 'mi' ? 0.000621371 : 0.001
+
+    if (!lat || !lng) {
+        next(new AppError
+            ('Please provide latitude and longtitude in format lat, lng',
+                400
+            ))
+    }
+    const distances = await Tour.aggregate([
+        {
+            $geoNear: {
+                near: {
+                    type: 'Point',
+                    coordinates: [lng * 1, lat * 1]
+                },
+                distanceField: 'distance',
+                distanceMultiplier: multiplier // to set the distance in km or miles
+            }
+        },
+        {
+            $project: { // to show only distance and name in output
+                distance: 1,
+                name: 1
+            }
+        }
+    ])
+
+    res.status(200).json({
+        status: 'success',
+        data: {
+            data: distances
+        }
+    })
 })
