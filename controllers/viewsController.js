4f4ecdbe0ae4b1119bb8abd6b1aa7fbd271bf823
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-09 16:34:21 +0200

    178 building the tour overview - part 1

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,8 +1,20 @@
-exports.getOverwiev = (req, res) => {
+
+const Tour = require('../models/tourModel')
+const cathcAsync = require('../utils/catchAsync')
+
+exports.getOverwiev = cathcAsync(async (req, res) => {
+    //1) get all tour ata from collection
+    const tours = await Tour.find()
+
+
+    //2) build template
+
+    //3)render template using tour data from point 1)
     res.status(200).render('overview', {
-        title: 'All tours'
+        title: 'All tours',
+        tours
     })
-}
+})
 
 exports.getTour = (req, res) => {
     res.status(200).render('tour', {
