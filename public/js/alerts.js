95dec9a850c2f75a88e6964d6c605ed75d51f3da
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-12 18:21:06 +0200

    187 logging in users with our API - part 3

---
+++ public/js/alerts.js
@@ -0,0 +1,13 @@
+export const hideAlert = () => {
+    const element = document.querySelector('.alert')
+    if (element) element.parentElement.removeChild(element)
+}
+
+// type 'success' or 'error'
+export const showAlert = (type, message) => {
+    hideAlert()
+
+    const markup = `<div class="alert alert--${type}">${message}</div>`
+    document.querySelector('body').insertAdjacentHTML('afterbegin', markup)
+    window.setTimeout(hideAlert, 5000)
+}
--- public/js/login.js
+++ public/js/login.js
@@ -1,4 +1,7 @@
-const login = async (email, password) => {
+import axios from 'axios'
+import { showAlert } from './alerts'
+
+export const login = async (email, password) => {
     try {
         const res = await axios({
             method: 'POST',
@@ -10,20 +13,13 @@ const login = async (email, password) => {
         })
 
         if(res.data.status === 'success'){
-            alert('Logged in succsessfully!')
+            showAlert('success','Logged in succsessfully!')
             window.setTimeout(()=>{
                 location.assign('/')
             }, 1500)
         }
         console.log(res);
     } catch (error) {
-        alert(error.response.data.message);
+        showAlert('error', error.response.data.message);
     }
 }
-
-document.querySelector('.form').addEventListener('submit', e => {
-    e.preventDefault()
-    const email = document.getElementById('email').value
-    const password = document.getElementById('password').value
-    login(email, password)
-})
