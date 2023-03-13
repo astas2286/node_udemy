23819eabe736f3cfed83de92c2e138bec2bf33a7
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-13 18:55:12 +0200

    192 updating user data with our API

---
+++ public/js/updateSettings.js
@@ -0,0 +1,22 @@
+import axios from 'axios'
+import { showAlert } from './alerts'
+
+export const updateData = async (name, email) => {
+    try {
+        const res = await axios({
+            method: 'PATCH',
+            url: 'http://127.0.0.1:8000/api/v1/users/updateMe/',
+            data: {
+                name,
+                email
+            }
+        })
+
+        if (res.data.status = 'success') {
+            showAlert('success', 'Data updated succsessfully!')
+        }
+        console.log(res);
+    } catch (error) {
+        showAlert('error', error.response.data.message);
+    }
+}
