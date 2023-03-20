a5f1f52337ff45e58db46c8c741684c47950969f
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-03-20 22:06:56 +0200

    typo

--- controllers/authController.js
+++ controllers/authController.js
@@ -64,7 +64,7 @@ exports.login = catchAsync(async (req, res, next) => {
         email
     }).select('+password')
 
-    if (!user || !await user.correctPassword(password, user.password)) {
+    if (!user || !(await user.correctPassword(password, user.password))) {
         return next(new AppError('Incorrect email or password', 401))
     }
     //3)If everything is OK, send token to client
