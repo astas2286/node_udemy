89b6c45c12ce8797be3be3c9872145fcaf04783d
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2023-04-03 18:40:22 +0300

    fixing the error message

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -7,8 +7,7 @@ const cathcAsync = require('../utils/catchAsync')
 exports.alerts = (req, res, next) => {
     const {alert}= req.query
     if(alert === 'booking'){
-        req.locals.alert = `Congrtas! You booking was successfull!\n 
-        If your booking doesn\`t show up here immidiatly, please come back later.`
+        req.locals.alert = "Congrtas! You booking was successfull! If your booking doesn't show up here immidiatly, please come back later."
         next()
     }
 }
